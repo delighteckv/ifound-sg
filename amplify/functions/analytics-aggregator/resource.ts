3644828a630a832ba30cb85d7ad91730beb6ae3c
Vishnu P <vishnu@delighteck.com>
2026-04-27 14:30:34 +0530

    added missing secret

--- amplify/functions/analytics-aggregator/resource.ts
+++ amplify/functions/analytics-aggregator/resource.ts
@@ -1,11 +1,9 @@
-import { defineFunction } from "@aws-amplify/backend";
+import { defineFunction, secret} from "@aws-amplify/backend";
 
 export const analyticsAggregator = defineFunction({
   name: "analytics-aggregator",
   entry: "./handler.ts",
   timeoutSeconds: 30,
   resourceGroupName: "data",
-
-
-  IFOUND_AWS_REGION
+  IFOUND_AWS_REGION: secret("IFOUND_AWS_REGION"),
 });
